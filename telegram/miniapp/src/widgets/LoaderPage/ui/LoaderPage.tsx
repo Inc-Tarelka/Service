2b7208dbd356c665188837743e1b589d7c41c769
gwizxs <gwizxs@gmail.com>
2026-01-01 18:16:51 +0300

    add color palette and add style lint in project

--- telegram/miniapp/src/widgets/LoaderPage/ui/LoaderPage.tsx
+++ telegram/miniapp/src/widgets/LoaderPage/ui/LoaderPage.tsx
@@ -1,30 +1,10 @@
-import { useMemo } from 'react';
 import s from './LoaderPage.module.scss';
-
-const loadingMessages = [
-  'Последние штрихи...',
-  'Связываемся с экзаменаторами...',
-  'Подбираем задания...',
-  'Советуемся с лингвистами...',
-  'Проверяем орфографию...',
-  'Договариваемся о правилах...',
-  'Настраиваем сложность...',
-  'Полируем формулировки...',
-  'Согласовываем детали...',
-];
-
 export const LoaderPage = () => {
-  const message = useMemo(
-    () => loadingMessages[Math.floor(Math.random() * loadingMessages.length)],
-    [],
-  );
-
   return (
     <div className={s.loaderPageBg}>
       <div className={s.loaderWrapper}>
         <div className={s.loaderContent}>
           <span className={s.loader}></span>
-          <p className={s.message}>{message}</p>
         </div>
       </div>
     </div>
