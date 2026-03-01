69a60850155c823d75f76457726dfad0a9f650ed
ilya <kisel.ds@gmail.com>
2026-03-01 12:53:22 +0300

    upd services search

--- api/internal/model/publication.go
+++ api/internal/model/publication.go
@@ -12,22 +12,24 @@ const (
 
 // Publication — сущность поста/публикации
 type Publication struct {
-	ID            int64              `json:"id" db:"id"`
-	AuthorID      int64              `json:"authorId" db:"author_id"`
-	Name          string             `json:"name" db:"name"`
-	Description   string             `json:"description" db:"description"`
-	Type          PublicationType    `json:"type" db:"type"`
-	CityID        *int64             `json:"cityId,omitempty" db:"city_id"`
-	Images        []PublicationImage `json:"images,omitempty"`
-	Tags          []PublicationTag   `json:"tags,omitempty"`
-	CoAuthors     []TarelkaUser      `json:"coAuthors,omitempty"`
-	Needs         []Need             `json:"needs,omitempty"`
-	LikesCount    int64              `json:"likesCount,omitempty"`
-	CommentsCount int64              `json:"commentsCount,omitempty"`
+	ID              int64              `json:"id" db:"id"`
+	AuthorID        int64              `json:"authorId" db:"author_id"`
+	AuthorFirstName string             `json:"authorFirstName" db:"author_first_name"`
+	AuthorLastName  string             `json:"authorLastName" db:"author_last_name"`
+	Name            string             `json:"name" db:"name"`
+	Description     string             `json:"description" db:"description"`
+	Type            PublicationType    `json:"type" db:"type"`
+	CityID          *int64             `json:"cityId" db:"city_id"`
+	Images          []PublicationImage `json:"images"`
+	Tags            []PublicationTag   `json:"tags"`
+	CoAuthors       []TarelkaUser      `json:"coAuthors"`
+	Needs           []Need             `json:"needs"`
+	LikesCount      int64              `json:"likesCount"`
+	CommentsCount   int64              `json:"commentsCount"`
 	// TopImageURL — URL изображения с приоритетом 1, если задано
-	TopImageURL *string `json:"topImageUrl,omitempty"`
+	TopImageURL *string `json:"topImageUrl"`
 	// AuthorTelegramURL — ссылка на Telegram автора, если задано
-	AuthorTelegramURL *string   `json:"authorTelegramUrl,omitempty" db:"telegram_url"`
+	AuthorTelegramURL *string   `json:"authorTelegramUrl" db:"telegram_url"`
 	CreatedAt         time.Time `json:"createdAt" db:"created_at"`
 }
 
